f24f4f3fa0ec21562e907eaa333a00851f06237f
steve.whitmore <steve@swhitmore.com>
2019-09-05 23:05:30 -0500

    It's implied python3 will be the version used. No need to kill chromium, just the driver. Renamed error handling script.

--- handle_py_script.sh
+++ handle_py_script.sh
@@ -3,8 +3,12 @@
 website=$1
 
 run_scan_for_updates_script() {
-    python3 ./scan_for_updates.py "$website"
+    python ./scan_for_updates.py "$website"
 }
 error_output=$(run_scan_for_updates_script 2>&1)
 
-./notify_of_results.sh "$error_output"
+pkill chromedriver
+
+./notify_errors.sh "$error_output"
+
+exit
