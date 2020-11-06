8e559e521643c87888db2f96f62c2a126aafac3f
stevewhitmore <s25w89@gmail.com>
2020-11-06 10:31:19 -0600

    Added MIT license. Organized files a little. Added versioning file

---
+++ scripts/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='wp-update-scanner', version='1.0', author='Steve Whitmore',
+      author_email='steve.whitmore@protonmail.com',
+      url='https://stevewhitmore.github.io',)
