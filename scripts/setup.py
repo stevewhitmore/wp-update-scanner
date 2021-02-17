e0f6f95e6d564617f241fb4ed9a8a71ab59a9166
stevewhitmore <s25w89@gmail.com>
2021-02-17 17:33:19 -0600

    Removed unnecessary file

--- scripts/setup.py
+++
@@ -1,7 +0,0 @@
-"""Tracks version of application
-"""
-from distutils.core import setup
-
-setup(name='wp-update-scanner', version='1.0', author='Steve Whitmore',
-      author_email='steve.whitmore@protonmail.com',
-      url='https://stevewhitmore.github.io',)
