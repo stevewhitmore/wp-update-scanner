c52a692d80041790affdd99ce78693b4aaa6a3a7
stevewhitmore <s25w89@gmail.com>
2021-03-02 10:23:04 -0600

    Email update even for no action needed

--- bin/notify_updates.sh
+++ bin/notify_updates.sh
@@ -3,8 +3,10 @@
 report_email=$(awk -F= '/email/ { print $2 }' ../config.txt)
 record_file=$(awk -F= '/recordFile/ { print $2 }' ../config.txt)
 
-if [ -n "$record_file" ]; then
-    mutt -s "WordPress Updates Needed" "$report_email" < "$record_file"
+if [[ -s "$record_file" ]]; then
+    mutt -s "wp-update-scanner: WordPress Updates Needed" "$report_email" < "$record_file"
+else
+    echo "Scan complete. No updates needed." | mutt -s "wp-update-scanner: No updates" "$report_email"
 fi
 
 exit
